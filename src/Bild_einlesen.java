730cdec1c2844b3dba9f5af16c76cf0d0538156d
KNapret <jonnycut@yahoo.de>
2015-09-22 07:39:47 +0200

    1st

---
+++ src/Bild_einlesen.java
@@ -0,0 +1,86 @@
+import sun.misc.HexDumpEncoder;
+import sun.nio.cs.US_ASCII;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by KNapret on 18.09.2015.
+ */
+public class Bild_einlesen {
+    public static void main(String[] args) {
+        Collection<String> al = new ArrayList<>();
+
+
+        try (BufferedInputStream reader = new BufferedInputStream(new FileInputStream("bild.jpg"))){
+            String puffer = null;
+            int zeile = 0;
+            while((zeile = reader.read())!= -1){
+
+                puffer = null;
+                puffer = Integer.toHexString(zeile);
+                if(puffer.length()==1) puffer = 0+ puffer;
+                al.add(puffer);
+            }
+        }
+
+
+            catch(IOException e){
+                System.out.println("Fehler beim Lesen");
+            }
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("hex_daten.txt"))){
+            int i=0;
+            int adresse = 0;
+            int umbruch = 0;
+            String ausgabe ="";
+
+            String hexAdresse = Integer.toHexString(adresse);
+
+            while(hexAdresse.length()<Integer.toHexString(al.size()).length()){
+                hexAdresse = "0"+hexAdresse;
+            }
+
+            writer.write("0x"+hexAdresse+"        ");
+
+
+            for(String s : al){
+
+                ausgabe =ausgabe +s;
+                writer.write(s.toUpperCase());
+                writer.write(" ");
+                i++;
+
+                if(i==16) {
+
+                    writer.write("        ");
+                    writer.write(new String(ausgabe));
+                    ausgabe = "";
+                    umbruch ++;
+                    i = 0;
+                    writer.newLine();
+                    adresse = adresse +16;
+                    if(umbruch ==16){
+                        writer.write("\n\n");
+                        umbruch =0;
+                    }
+                    hexAdresse=Integer.toHexString(adresse);
+                    while(hexAdresse.length()<Integer.toHexString(al.size()).length()){
+                        hexAdresse = "0"+hexAdresse;
+                    }
+                        writer.write("0x"+hexAdresse+ "        ");
+
+
+                }
+            }
+
+
+        }
+
+        catch(IOException e){
+            System.out.println("Fehler beim Schreiben!");
+        }
+    }//end MainFunction
+}//end MainClass
