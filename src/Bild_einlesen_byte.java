5dab3be7c9ce2e1a57549aff1ab5d3c161db323d
KNapret <jonnycut@yahoo.de>
2015-09-25 10:34:41 +0200

    ToDo Added

--- src/Bild_einlesen_byte.java
+++ src/Bild_einlesen_byte.java
@@ -34,50 +34,8 @@ public class Bild_einlesen_byte {
                 System.out.println("Fehler beim Lesen");
             }
 
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("hex_daten.txt"))){
-            int i=0;
-            int adresse = 0;
-            int umbruch = 0;
-            String ausgabe ="";
-
-            String hexAdresse = Integer.toHexString(adresse);
-
-            while(hexAdresse.length()<Integer.toHexString(al.size()).length()){
-                hexAdresse = "0"+hexAdresse;
-            }
-
-            writer.write("0x"+hexAdresse+"        ");
-            //test
-
-            for(Byte s : al){
-
-                ausgabe =ausgabe +s;
-                writer.write(s.toString());
-                writer.write(" ");
-                i++;
-
-                if(i==16) {
-
-                    writer.write("        ");
-                    writer.write(new String(ausgabe));
-                    ausgabe = "";
-                    umbruch ++;
-                    i = 0;
-                    writer.newLine();
-                    adresse = adresse +16;
-                    if(umbruch ==16){
-                        writer.write("\n\n");
-                        umbruch =0;
-                    }
-                    hexAdresse=Integer.toHexString(adresse);
-                    while(hexAdresse.length()<Integer.toHexString(al.size()).length()){
-                        hexAdresse = "0"+hexAdresse;
-                    }
-                        writer.write("0x"+hexAdresse+ "        ");
-
-
-                }
-            }
+        try(BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream("hex_daten.txt"))){
+            //ToDo: schreiben in Datei
 
 
         }
