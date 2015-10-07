248cac89ab166f3137657e8318beefd7de59f48b
JonnyCut <jonnycut@yahoo.de>
2015-10-07 20:19:03 +0200

    SOP  HA Kw47 Fertig, inkl, Kommentare

--- src/HA_Kw41_Bibel_woerter.java
+++ src/HA_Kw41_Bibel_woerter.java
@@ -1,10 +1,10 @@
+import sun.awt.image.ImageWatched;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,10 +17,10 @@ public class HA_Kw41_Bibel_woerter {
 
         Map<String, Integer> allWords = new HashMap<>();
 
-
+        long time = System.currentTimeMillis();
         try (BufferedReader br = new BufferedReader(new FileReader("bibel.txt"))) {
 
-            Pattern p = Pattern.compile("\\b\\p{Alpha}+"); //kompiliert einen regulären Ausdruck aus dem gegebenen Muster
+            Pattern p = Pattern.compile("\\b[a-zA-ZüäöÜÄÖß]+"); //kompiliert einen regulären Ausdruck aus dem gegebenen Muster
             //Pattern p = Pattern.compile("(^| |-)[aA]n[a-zäüöß]+");
             //                            (Anfang der Zeile OR Leerzeichen OR "bindestrich" FOLLOWEDBY[=FB] a OR A FB n AND[a bis z inkl. Sonderzeichen] + = mind. ein Buchstabe
             int counter = 0;
@@ -30,7 +30,7 @@ public class HA_Kw41_Bibel_woerter {
                 Matcher m = p.matcher(zeile); //nimmt den von p kompilierten Audruck und ueberprueft das "matching"
 
                 while (m.find()) {
-                    String tmp = m.group();
+                    String tmp = m.group().toLowerCase();
 
                     if (allWords.containsKey(tmp) == false) {
 
@@ -42,11 +42,30 @@ public class HA_Kw41_Bibel_woerter {
                 }
 
 
-
-
             }
 
+            //LinkedList aus dem EntrySet (enthält Key und Value --> object.getKey(); oder object.getValue();
+            LinkedList<Map.Entry<String, Integer>> tmpList = new LinkedList<>(allWords.entrySet());
+
+            //Liste sortieren mit eigenem (new) comparator --> Wenn Value gleich, dann nach Key (string)
+            Collections.sort(tmpList, new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    if (o2.getValue() - o1.getValue() == 0)
+                        return o2.getKey().compareTo(o1.getKey());
+
+                    return o1.getValue() - o2.getValue();
+                }
+            });
+            //
+            //da Liste aus entryset, kann direkt ausgegeben werden.
+            //zum weiteren Bearbeiten müsste aus der Liste wieder eine Map gebaut werden
+
+
+            System.out.println(tmpList);
+
             System.out.println(allWords.size());
+            System.out.println(System.currentTimeMillis() - time);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
